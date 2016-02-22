d628be89c462c78a89024ee4d8a21de1481c964b
0x434D53 <christoph.seufert@gmail.com>
2016-02-22 12:39:40 +0100

    params added for ServerAndListenTLS

--- server/server.go
+++ server/server.go
@@ -10,9 +10,13 @@ import (
 
 // ListenAndServeTLS creates a new server like http.ListenAndServeTLS but creates a self-signed certificate on the fly.
 // Warning: Since it's not a trusted certificate chain, the golang http-server will log http2: server: error reading preface... when connecting to the server.
-func ListenAndServeTLS(addr string, handler http.Handler) error {
+// If p is nil the Default Parameters (RSA2048, Valid from now for 365 days) will be used for the certificate generation
+func ListenAndServeTLS(addr string, handler http.Handler, p *certgen.CertParams) error {
 	srv := &http.Server{Addr: addr, Handler: handler}
-	cert, key, err := certgen.GenerateToMemory(certgen.NewDefaultParams())
+	if p == nil {
+		p = certgen.NewDefaultParams()
+	}
+	cert, key, err := certgen.GenerateToMemory(p)
 	if err != nil {
 		return err
 	}
